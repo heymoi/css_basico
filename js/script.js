6d9a090b51b70c655db2b9bdd78736b527eacc59
Raúl Márquez <36087497+heymoi@users.noreply.github.com>
2020-04-22 07:02:06 -0600

    Creando el repositorio y agregando los archivos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*
+document.getElementById('practicas').onload = function onclick(event) {
+  AdjustIframeHeightOnLoad()
+}
+function AdjustIframeHeightOnLoad()
+{
+  var alto = document.getElementById("practicas").contentWindow.document.body.scrollHeight +20;
+  document.getElementById("practicas").style.height = alto + "px";
+}
+*/
